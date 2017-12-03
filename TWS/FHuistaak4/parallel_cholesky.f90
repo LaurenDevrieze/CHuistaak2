cd7bf5082bb01905bea65a03caf1741e7ffe6f69
Lauren Devrieze <Lauren.Devrieze@student.kuleuven.be>
2017-12-03 12:27:03 +0100

    Aanpassing pi en parallel_cholesky

--- TWS/FHuistaak4/parallel_cholesky.f90
+++ TWS/FHuistaak4/parallel_cholesky.f90
@@ -1,4 +1,15 @@
 program parallel_cholesky
+!Lauren Devrieze
+
+!Commando's: gfortran -o parallel parallel_cholesky.f90
+!			 mpirun -n p parallel
+!				met p aantal processen
+
+!Tijdsbesteding: x!!! uur
+
+!Bespreking: Om te controleren of de processen wel parallel lopen checken
+!			 de print statements. Deze zijn niet in de verwachte volgorde
+!			 moesten de processen sequentieel lopen.
 
 use mpi
 
@@ -8,8 +19,9 @@ integer, parameter :: N = 10
 real, parameter :: pi = 4*atan(1.0)
 complex, dimension(:,:), allocatable :: H, L
 complex, dimension(:), allocatable :: matlin
-real :: b,c
-integer :: f,i,j,info, error, id
+real :: b,c, som
+integer :: f ,i ,j, info, error, id, tag, status, p
+logical :: juist, alJuist
 
 call mpi_init(error)
 
@@ -17,16 +29,18 @@ allocate(H(N,N))
 allocate(L(N,N))
 allocate(matlin(N*(N+1)/2))
 
+!Genereer H
 do i = 1,N
 	do j = 1,N
 		call random_number(b)
 		call random_number(c)
 		H(i,j) = sqrt(-2*log(b))*exp(2*pi*cmplx(0.0,1.0)*c)
 	end do
-end do
-	
+end do	
 H = H*transpose(conjg(H))
 
+!Plaats H in een array met 1 dimensie om the cholesky compositie 
+!te kunnen uitvoeren
 f = 1
 do j = 1,N
 	do i = j,N
@@ -35,8 +49,10 @@ do j = 1,N
 	end do
 end do
 
+!Voer de cholesky decompositie uit
 call cpptrf('L',N,matlin,info)
 
+!Plaats de resultaten in een benedendriehoek matrix L
 f = 1
 do j = 1,N
 	do i = j,N
@@ -45,9 +61,35 @@ do j = 1,N
 	end do
 end do
 
+!Bereken de som van de elementen strikt boven de diagonaal van L
+som = 0
+do j = 2,N
+	do i = 1,j-1
+		som = som + L(i,j) 
+	end do
+end do
+
+!Check of L juist is
+H = L*transpose(conjg(L))
+
+!Print voor ieder proces informatie af.
 call mpi_comm_rank(mpi_comm_world,id,error)
-print *, 'proces: ' , id
-print *, 'Eerste element H: ' , H(1,1)
+print *, 'proces: ' , id, 'met eerste element H:', H(1,1) , 'en som boven de diagonaal van L: ' , som
+if(id == 1) then
+	alJuist = juist
+	call mpi_comm_size(mpi_comm_world,p,error)
+	do i = 2,p
+		call MPI_Recv(juist,1,MPI_LOGICAL,i,tag,MPI_COMM_WORLD, error )
+		if(.not. juist) alJuist = .false.
+	end
+	if(alJuist) then
+		print *, 'Proces ' , id , ': alle processen geven een juist resultaat.'
+	else
+		print *, 'Proces ' , id , ': er zijn processen die geen juist resultaat geven.'
+	endif
+else
+	call MPI_Send(juist,1,MPI_LOGICAL,1,tag,MPI_COMM_WORLD, error )
+endif
 
 call MPI_Finalize(error)
 
--- TWS/FHuistaak4/pi.f90
+++ TWS/FHuistaak4/pi.f90
@@ -1,4 +1,33 @@
 program pi
+!Lauren Devrieze
+
+!Commando's: gfortran -I FM_files/ FM_files/*.o pi.f90
+
+!Tijdsbesteding: x!!!! uur
+
+!Bespreking: Als recusieve formule wordt: s_2k = sqrt((1.0-sqrt(1.0-s_k*s_k))/2.0)
+!			 gebruikt. Om tot deze formule te komen wordt in figuur 1 pythagoras
+!			 gebruikt voor de 2 rechthoekige driehoeken met de variabelen a en b.
+!			 Als de waarden a en b weggewerkt worden door substitutie wordt de
+!			 volgende formule bekomen.
+
+!			 Bij enkele precisie zal de fout eerst kleiner worden tot ongeveer orde
+!			 10^-6, dan zal de fout stijgen en bij stap 12 is de benadering van
+!			 pi zelf 0. Dit komt omdat de berekende s_k zo klein zal zijn dat enkele
+!			 precisie het getal niet meer goed kan voorstellen. s_k zal steeds fouter
+!			 worden tot het uiteindelijk 0 wordt.
+
+!			 Bij multiprecisie gebeurt dit niet en zal pi steeds beter benaderd worden
+!			 zolang de nieuwe s_k kan voorgesteld worden met de precisie. Een reden om
+!			 multiprecisie niet te gebruiken is hoe lang het duurt.
+!			 Timings voor x!!!! stappen:
+!			 enkele precisie:
+!			 multiprecisie: 									!timing nog invullen
+!			 Dit komt doordat eerst en vooral omdat multiprecisie meer cijfers gebruikt
+!			 om berekeningen te doen dus dat ze daarom langer duren. Ook het feit dat het
+!			 geen standaard type is dat in fortran zit zal vermoedelijk voor vertraging 
+!			 zorgen. Als pi niet zo goed benadert hoeft te worden is het beter om enkele
+!			 precisie te gebruiken. 
 
 use fmzm
 
@@ -30,25 +59,19 @@ subroutine calculatePi()
   s = sqrt(2.0)/2.0
   do i = 3,30
 		s = sqrt((1.0-sqrt(1.0-s*s))/2.0)
-		!print *, 2**(i)*s
-		print *, 'Error: '
 		call fm_print(abs(2**(i)*s-pi))
-		print *, ' '
 	end do
 end subroutine
 
 subroutine calculatePi_fm()
-	type(fm) :: s,pi
+  type(fm) :: s,pi
   integer :: i
   pi = to_fm(4.0*atan(to_fm(1.0)))
   s = sqrt(to_fm(2.0))/to_fm(2.0)
   do i = 3,30
 		s = sqrt((to_fm(1.0)-sqrt(to_fm(1.0)-s*s))/to_fm(2.0))
-		!call fm_print(2**(i)*s)
-		print *, 'Error: ' 
 		call fm_print(abs(2**(i)*s-pi))
-		print *, ' '
-	end do
+  end do
 end subroutine
 
 end program
