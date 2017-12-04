b8e510c5b157284a8385fabb89ec0ec3a8321f50
LaurenDev <lauren.devrieze@student.kuleuven.be>
2017-12-04 17:17:34 +0100

    FHuistaak4 af

--- TWS/FHuistaak4/parallel_cholesky.f90
+++ TWS/FHuistaak4/parallel_cholesky.f90
@@ -1,16 +1,38 @@
 program parallel_cholesky
 !Lauren Devrieze
 
-!Commando's: gfortran -o parallel parallel_cholesky.f90
+!Commando's: gfortran -o parallel parallel_cholesky.f90 -llapack
 !			 mpirun -n p parallel
 !				met p aantal processen
 
-!Tijdsbesteding: x!!! uur
+!Tijdsbesteding: 3 uur
 
-!Bespreking: Om te controleren of de processen wel parallel lopen checken
+!Bespreking: Om te controleren of de processen wel parallel lopen checken we
 !			 de print statements. Deze zijn niet in de verwachte volgorde
-!			 moesten de processen sequentieel lopen.
+!			 moesten de processen sequentieel lopen en bij iedere uitvoering
+!			 is de volgorde anders. Het print statement dat aangeeft of de processen
+!			 juist zijn wordt laatst weergegeven omdat ieder proces de info moet
+!			 sturen naar proces 0 voor dit kan afgeprint worden.
 
+!			 Om een random nummer voor de parallele processen te generen werdt een stuk
+!			 code gebruikt van: https://www.linuxquestions.org/questions/programming-9/fortran-90-how-do-i-use-random_number-and-random_seed-913870/
+!			 dit geeft soms nog dezelfde getallen voor verschillende processen omdat
+!			 ze rond dezelfde tijd uitgevoerd worden.
+
+!Voorbeeld output:
+! proces:           3 met eerste element H: (  11.8205795    ,  0.00000000    ) en som boven de diagonaal van L:    0.00000000    
+! proces:           5 met eerste element H: (  11.8205795    ,  0.00000000    ) en som boven de diagonaal van L:    0.00000000    
+! proces:           0 met eerste element H: (  11.8205795    ,  0.00000000    ) en som boven de diagonaal van L:    0.00000000    
+! proces:           1 met eerste element H: (  96.8949814    ,  0.00000000    ) en som boven de diagonaal van L:    0.00000000    
+! proces:           4 met eerste element H: (  96.8949814    ,  0.00000000    ) en som boven de diagonaal van L:    0.00000000    
+! proces:           2 met eerste element H: (  96.8949814    ,  0.00000000    ) en som boven de diagonaal van L:    0.00000000    
+! proces:           7 met eerste element H: (  15.5243406    ,  0.00000000    ) en som boven de diagonaal van L:    0.00000000    
+! proces:           8 met eerste element H: (  15.5243406    ,  0.00000000    ) en som boven de diagonaal van L:    0.00000000    
+! proces:           6 met eerste element H: (  1.79431629    ,  0.00000000    ) en som boven de diagonaal van L:    0.00000000    
+! proces:           9 met eerste element H: (  19.5601711    ,  0.00000000    ) en som boven de diagonaal van L:    0.00000000    
+! Proces           0 : alle processen geven een juist resultaat.
+
+ 
 use mpi
 
 implicit none
@@ -20,8 +42,10 @@ real, parameter :: pi = 4*atan(1.0)
 complex, dimension(:,:), allocatable :: H, L
 complex, dimension(:), allocatable :: matlin
 real :: b,c, som
-integer :: f ,i ,j, info, error, id, tag, status, p
+integer :: f ,i ,j, info, error, id, tag, status(mpi_status_size), p,m
 logical :: juist, alJuist
+integer, dimension(8) :: value
+integer, dimension(:), allocatable :: seed
 
 call mpi_init(error)
 
@@ -32,12 +56,21 @@ allocate(matlin(N*(N+1)/2))
 !Genereer H
 do i = 1,N
 	do j = 1,N
+		call date_and_time(values = value)
+		call random_seed(size = m)
+  		allocate(seed(m))
+		seed(:) = value(8)
+		call random_seed(put=seed)
 		call random_number(b)
+		call date_and_time(values = value)
+		seed(:) = value(8)
+		call random_seed(put=seed)
 		call random_number(c)
+		deallocate(seed)
 		H(i,j) = sqrt(-2*log(b))*exp(2*pi*cmplx(0.0,1.0)*c)
 	end do
 end do	
-H = H*transpose(conjg(H))
+H = matmul(H,transpose(conjg(H)))
 
 !Plaats H in een array met 1 dimensie om the cholesky compositie 
 !te kunnen uitvoeren
@@ -70,25 +103,30 @@ do j = 2,N
 end do
 
 !Check of L juist is
-H = L*transpose(conjg(L))
+L = matmul(L,transpose(conjg(L)))
+juist = .false.
+if(maxval(abs(H-L)) < 0.00001) then
+	juist = .true.
+endif
+
 
 !Print voor ieder proces informatie af.
 call mpi_comm_rank(mpi_comm_world,id,error)
-print *, 'proces: ' , id, 'met eerste element H:', H(1,1) , 'en som boven de diagonaal van L: ' , som
-if(id == 1) then
+print *, 'proces:' , id, 'met eerste element H:', H(1,1) , 'en som boven de diagonaal van L: ' , som
+if(id .ne. 0) then
+	call MPI_Send(juist,1,MPI_LOGICAL,0,id,MPI_COMM_WORLD, error )	
+else
 	alJuist = juist
 	call mpi_comm_size(mpi_comm_world,p,error)
-	do i = 2,p
-		call MPI_Recv(juist,1,MPI_LOGICAL,i,tag,MPI_COMM_WORLD, error )
+	do i = 1,p-1
+		call mpi_recv(juist,1,MPI_LOGICAL,i,i,MPI_COMM_WORLD, status,error )
 		if(.not. juist) alJuist = .false.
-	end
+	end do
 	if(alJuist) then
-		print *, 'Proces ' , id , ': alle processen geven een juist resultaat.'
+		print *, 'Proces' , id , ': alle processen geven een juist resultaat.'
 	else
-		print *, 'Proces ' , id , ': er zijn processen die geen juist resultaat geven.'
-	endif
-else
-	call MPI_Send(juist,1,MPI_LOGICAL,1,tag,MPI_COMM_WORLD, error )
+		print *, 'Proces' , id , ': er zijn processen die geen juist resultaat geven.'
+	endif	
 endif
 
 call MPI_Finalize(error)
--- TWS/FHuistaak4/pi.f90
+++ TWS/FHuistaak4/pi.f90
@@ -1,9 +1,9 @@
 program pi
 !Lauren Devrieze
 
-!Commando's: gfortran -I FM_files/ FM_files/*.o pi.f90
+!Commando's: gfortran -I FM_files/ FM_files/*.o -o pi pi.f90
 
-!Tijdsbesteding: x!!!! uur
+!Tijdsbesteding: 3.5 uur
 
 !Bespreking: Als recusieve formule wordt: s_2k = sqrt((1.0-sqrt(1.0-s_k*s_k))/2.0)
 !			 gebruikt. Om tot deze formule te komen wordt in figuur 1 pythagoras
@@ -12,7 +12,7 @@ program pi
 !			 volgende formule bekomen.
 
 !			 Bij enkele precisie zal de fout eerst kleiner worden tot ongeveer orde
-!			 10^-6, dan zal de fout stijgen en bij stap 12 is de benadering van
+!			 10^-5, dan zal de fout stijgen en bij stap 12 is de benadering van
 !			 pi zelf 0. Dit komt omdat de berekende s_k zo klein zal zijn dat enkele
 !			 precisie het getal niet meer goed kan voorstellen. s_k zal steeds fouter
 !			 worden tot het uiteindelijk 0 wordt.
@@ -20,20 +20,62 @@ program pi
 !			 Bij multiprecisie gebeurt dit niet en zal pi steeds beter benaderd worden
 !			 zolang de nieuwe s_k kan voorgesteld worden met de precisie. Een reden om
 !			 multiprecisie niet te gebruiken is hoe lang het duurt.
-!			 Timings voor x!!!! stappen:
-!			 enkele precisie:
-!			 multiprecisie: 									!timing nog invullen
+!			 Timings voor 10⁵ stappen:
+!			 enkele precisie: 8.00000038E-03
+!		     multiprecisie: 0.208000004
 !			 Dit komt doordat eerst en vooral omdat multiprecisie meer cijfers gebruikt
 !			 om berekeningen te doen dus dat ze daarom langer duren. Ook het feit dat het
 !			 geen standaard type is dat in fortran zit zal vermoedelijk voor vertraging 
 !			 zorgen. Als pi niet zo goed benadert hoeft te worden is het beter om enkele
 !			 precisie te gebruiken. 
 
+!Output:
+! Enkele precisie:
+!       8.0125244455882105650143383279502884197169399375106M-2
+!       2.0148189859934840025143383279502884197169399375106M-2
+!       5.0465186410627697126433832795028841971693993751058M-3
+!       1.2592395120344494001433832795028841971693993751058M-3
+!       3.0699570710280877514338327950288419716939937510582M-4
+!       7.3822336741480650143383279502884197169399375105821M-5
+!       3.8472016388991815014338327950288419716939937510582M-4
+!       8.5863272612473028735661672049711580283060062489418M-4
+!       8.5863272612473028735661672049711580283060062489418M-4
+!       2.0685044927052464662356616720497115802830600624894M-2
+!       2.0685044927052464662356616720497115802830600624894M-2
+!       3.1316557725007155877514338327950288419716939937511M-1
+!       3.1415926535897932384626433832795028841971693993751M+0
+!       3.1415926535897932384626433832795028841971693993751M+0
+!       3.1415926535897932384626433832795028841971693993751M+0
+!       3.1415926535897932384626433832795028841971693993751M+0
+!       3.1415926535897932384626433832795028841971693993751M+0
+!       3.1415926535897932384626433832795028841971693993751M+0
+! timing:    0.00000000    
+! Meervoudige precisie:
+!       8.0125194669075064634963511036311950106412899490089M-2
+!       2.0147501331740952890085487647147029354103515343829M-2
+!       5.0441630438539746483853388429638166407958580150877M-3
+!       1.2614966350403261455248589478127520534661657269189M-3
+!       3.1540265702037040062361249128847581750613672531669M-4
+!       7.8852445492162134128323822680576261855583882177811M-5
+!       1.9713222701854326843273008741454663531604870745190M-5
+!       4.9283126335378337891205775841447978885408002625343M-6
+!       1.2320785932644646716195386689284496067492889250233M-6
+!       3.0801967549612226738912213295389196332372390201293M-7
+!       7.7004920572781037291041627574465116120943967839810M-8
+!       1.9251230249367165119651711590243137228626647249200M-8
+!       4.8128075689775354140023258084994033974687851972100M-9
+!       1.2032018926591178622215389998585141552006847377800M-9
+!       3.0080047319070034110576288073659336184945007061000M-10
+!       7.5200118299295139998422451545509879552262971990000M-11
+!       1.8800029574925038419730769505651884024382022340000M-11
+!       4.7000073937375879436905349527612051418231684500000M-12
+! timing:    0.00000000 
+
 use fmzm
 
 implicit none
 
-real(8) :: start, start_fm, finish, finish_fm
+real :: start, finish
 
 call fm_set(50)
 
@@ -43,11 +85,11 @@ call calculatePi()
 call cpu_time(finish)
 print *, 'timing: ' , finish-start
 
-call cpu_time(start_fm)
+call cpu_time(start)
 print *, 'Meervoudige precisie:'
 call calculatePi_fm()
-call cpu_time(finish_fm)
-print *, 'timing: ' , finish_fm-start_fm
+call cpu_time(finish)
+print *, 'timing: ' , finish-start
 
 contains
 
@@ -57,10 +99,11 @@ subroutine calculatePi()
   integer :: i
   pi = to_fm(4.0*atan(to_fm(1.0)))
   s = sqrt(2.0)/2.0
-  do i = 3,30
+  do i = 3,20
 		s = sqrt((1.0-sqrt(1.0-s*s))/2.0)
+		!print de fout
 		call fm_print(abs(2**(i)*s-pi))
-	end do
+  end do
 end subroutine
 
 subroutine calculatePi_fm()
@@ -68,8 +111,9 @@ subroutine calculatePi_fm()
   integer :: i
   pi = to_fm(4.0*atan(to_fm(1.0)))
   s = sqrt(to_fm(2.0))/to_fm(2.0)
-  do i = 3,30
+  do i = 3,20
 		s = sqrt((to_fm(1.0)-sqrt(to_fm(1.0)-s*s))/to_fm(2.0))
+		!print de fout
 		call fm_print(abs(2**(i)*s-pi))
   end do
 end subroutine
