6c559310ed912c5117f79927a0aa1db44bff645f
Lauren Devrieze <Lauren.Devrieze@student.kuleuven.be>
2018-01-02 09:59:23 +0100

    CHuistaak8

--- TWS/CHuistaak8/srda.cpp
+++ TWS/CHuistaak8/srda.cpp
@@ -107,7 +107,10 @@ int main(int argc, char *argv[]) {
   tws::matrix<double> X(N,N,1.0);
   
   auto xtx_op = [X,beta]( auto const& x, auto& y ){
+	std::cout<<X.num_columns()<<std::endl;
+	std::cout<<x.size()<<std::endl;
 	y = multiply(transpose(X),multiply(X,x)) + beta*x;
+	
 	};
 
   //TODO: Create xtx_op, use your knowledge from C++2
